d05c85f89fd92a42108664293600af35058bee91
DarthMuzammil <muzammil.inamdar98@gmail.com>
2021-06-15 00:36:26 +0530

    Assignment1 OOC

--- OOC/OOC1_2KE19CS053.java
+++ OOC/OOC1_2KE19CS053.java
@@ -1,30 +1,63 @@
+
 /*
 Author : Mohammed Muzammil
 Java Assignment 
 Semester IV
 */
 import java.util.Scanner;
-public class OOC1_2KE19CS053{
-    public static void main(String[] args){
-        //Variable Declarations
+
+public class OOC1_2KE19CS053 {
+    public static void main(String[] args) {
+        // Variable Declarations
         Scanner reader = new Scanner(System.in);
-        int choice;
-        int op1,op2,result;
-        System.out.println("\nCHOOSE ONE OF THE ARITHMETIC OPERATIONS\n1. Addition\n2. Subtraction \n3. Multiplication \n4. Division \n5. Modulus\n");
-        choice = reader.nextInt();
-        System.out.println("\nEnter the Operands\n");
-        System.out.println("\nOperand 1 : ");
-        op1 = reader.nextInt();
-        System.out.println("\nOperand 2 : ");
-        op2 = reader.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("\nADDITION\n");
-                
-                break;
-        
-            default:
-                break;
+        int choice = 0;
+        int op1, op2;
+        double result;
+        while (choice != 6) {
+            System.out.println(
+                    "\nCHOOSE ONE OF THE ARITHMETIC OPERATIONS\n1. Addition\n2. Subtraction \n3. Multiplication \n4. Division \n5. Remainder\n6. Exit\n");
+            choice = reader.nextInt();
+            if (choice != 6) {
+                System.out.println("\nEnter the Operands\n");
+                System.out.println("\nOperand 1 : ");
+                op1 = reader.nextInt();
+                System.out.println("\nOperand 2 : ");
+                op2 = reader.nextInt();
+                switch (choice) {
+                    case 1:
+                        System.out.println("\nADDITION\n");
+                        result = op1 + op2;
+                        System.out.println(op1 + " + " + op2 + " = " + result);
+                        break;
+                    case 2:
+                        System.out.println("\nSUBTRACTION\n");
+                        result = op1 - op2;
+                        System.out.println(op1 + " - " + op2 + " = " + result);
+                        break;
+                    case 3:
+                        System.out.println("\nMULTIPLICATION\n");
+                        result = op1 * op2;
+                        System.out.println(op1 + " * " + op2 + " = " + result);
+                        break;
+                    case 4:
+                        System.out.println("\nDIVISION\n");
+                        try {
+                            result = (float)op1 / (float)op2;
+                            System.out.println(op1 + " / " + op2 + " = " + result);
+                        } catch (ArithmeticException e) {
+                            System.out.println("\nCANNOT DIVIDE BY 0!\n");
+                        }
+                        break;
+                    case 5:
+                        System.out.println("\nRemainder\n");
+                        result = op1 % op2;
+                        System.out.println(op1 + " % " + op2 + " = " + result);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+        reader.close();
     }
 }
