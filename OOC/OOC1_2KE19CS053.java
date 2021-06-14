8d3616eb409fecf9fd0179647942ef0bd41e94f9
DarthMuzammil <muzammil.inamdar98@gmail.com>
2021-06-15 01:48:06 +0530

    changes

--- OOC/OOC1_2KE19CS053.java
+++ OOC/OOC1_2KE19CS053.java
@@ -61,3 +61,155 @@ public class OOC1_2KE19CS053 {
         reader.close();
     }
 }
+/*
+OUTPUT :
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+1
+
+Enter the Operands
+
+
+Operand 1 : 
+1
+
+Operand 2 : 
+2
+
+ADDITION
+
+1 + 2 = 3
+
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+2
+
+Enter the Operands
+
+
+Operand 1 : 
+1
+
+Operand 2 : 
+2
+
+SUBTRACTION
+
+1 - 2 = -1
+
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+3
+
+Enter the Operands
+
+
+Operand 1 : 
+2
+
+Operand 2 : 
+2
+
+MULTIPLICATION
+
+2 * 2 = 4
+
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+4
+
+Enter the Operands
+
+
+Operand 1 : 
+1
+
+Operand 2 : 
+0
+
+DIVISION
+
+
+CANNOT DIVIDE BY 0!
+
+
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+4
+
+Enter the Operands
+
+
+Operand 1 : 
+4
+
+Operand 2 : 
+2
+
+DIVISION
+
+4 / 2 = 2
+
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+5
+
+Enter the Operands
+
+
+Operand 1 : 
+5
+
+Operand 2 : 
+2
+
+Remainder
+
+5 % 2 = 1
+
+CHOOSE ONE OF THE ARITHMETIC OPERATIONS
+1. Addition
+2. Subtraction 
+3. Multiplication 
+4. Division 
+5. Remainder
+6. Exit
+
+6
+*/
--- OOC/OOC2_2KE19CS053.java
+++ OOC/OOC2_2KE19CS053.java
@@ -4,65 +4,198 @@ Java Assignment
 Semester IV
 */
 import java.util.Scanner;
-public class OOC2_2KE19CS053{
-    public static char enterchar(){
+
+public class OOC2_2KE19CS053 {
+    public static char enterchar(Scanner input) {
         char ch;
-        Scanner input = new Scanner(System.in);
         System.out.println("\nENTER A CHARACTER : ");
         ch = input.next().charAt(0);
-        input.close();
         return Character.toLowerCase(ch);
     }
-    public static void main(String[] args){
-        Scanner  reader = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
         int choice = 0;
         char ch;
-        while(choice != 5){
-        System.out.println("\nCHARACTER CHECK:\n1. VOWEL\n2. CONSONANT\n3. SPECIAL CHARACTER\n4. NUMBER\n5. EXIT");
-        choice = reader.nextInt();
-        if(choice != 5){
-            switch (choice) {
-                case 1:
-                    ch = enterchar();
-                    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
-                        System.out.println("\n" + ch + " is a vowel\n");
-                    }
-                    else{
-                        System.out.println("\n" + ch + " is not a vowel\n");
-                    }
-                    break;
-                case 2:
-                    ch = enterchar();
-                    if((ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') && ch > 'a' && ch < 'z'){
-                        System.out.println("\n" + ch + " is a consonant\n");
-                    }
-                    else{
-                        System.out.println("\n" + ch + " is not a consonant\n");
-                    }
-                    break;
-                case 3:
-                    ch = enterchar();
-                    if(!(ch >= 'a' && ch <= 'z') && !(ch > '0' && ch < '9')){
-                        System.out.println("\n" + ch + " is a special character\n");
-                    }
-                    else{
-                        System.out.println("\n" + ch + " is not a special character\n");
-                    }
-                    break;
-                case 4:
-                    ch = enterchar();
-                    if(ch >= '0' && ch <= '9'){
-                        System.out.println("\n" + ch + " is a number\n");
-                    }
-                    else{
-                        System.out.println("\n" + ch + " is not a number\n");
-                    }
-                    break;
-                default:
-                    break;
+        while (choice != 5) {
+            choice = 0;
+            System.out.println("\nCHARACTER CHECK:\n1. VOWEL\n2. CONSONANT\n3. SPECIAL CHARACTER\n4. NUMBER\n5. EXIT");
+            choice = reader.nextInt();
+            if (choice != 5) {
+                switch (choice) {
+                    case 1:
+                        ch = enterchar(reader);
+                        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                            System.out.println("\n" + ch + " is a vowel\n");
+                        } else {
+                            System.out.println("\n" + ch + " is not a vowel\n");
+                        }
+                        break;
+                    case 2:
+                        ch = enterchar(reader);
+                        if ((ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') && ch > 'a' && ch < 'z') {
+                            System.out.println("\n" + ch + " is a consonant\n");
+                        } else {
+                            System.out.println("\n" + ch + " is not a consonant\n");
+                        }
+                        break;
+                    case 3:
+                        ch = enterchar(reader);
+                        if (!(ch >= 'a' && ch <= 'z') && !(ch > '0' && ch < '9')) {
+                            System.out.println("\n" + ch + " is a special character\n");
+                        } else {
+                            System.out.println("\n" + ch + " is not a special character\n");
+                        }
+                        break;
+                    case 4:
+                        ch = enterchar(reader);
+                        if (ch >= '0' && ch <= '9') {
+                            System.out.println("\n" + ch + " is a number\n");
+                        } else {
+                            System.out.println("\n" + ch + " is not a number\n");
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
-    }
         reader.close();
     }
 }
+/*
+OUTPUT:
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+1
+
+ENTER A CHARACTER : 
+a
+
+a is a vowel
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+1
+
+ENTER A CHARACTER : 
+q
+
+q is not a vowel
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+2
+
+ENTER A CHARACTER : 
+e
+
+e is not a consonant
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+2
+
+ENTER A CHARACTER : 
+b
+
+b is a consonant
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+3
+
+ENTER A CHARACTER : 
+w
+
+w is not a special character
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+3
+
+ENTER A CHARACTER : 
+#
+
+# is a special character
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+4
+
+ENTER A CHARACTER : 
+@
+
+@ is not a number
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+4
+
+ENTER A CHARACTER : 
+h
+
+h is not a number
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+4
+
+ENTER A CHARACTER : 
+3
+
+3 is a number
+
+
+CHARACTER CHECK:
+1. VOWEL
+2. CONSONANT
+3. SPECIAL CHARACTER
+4. NUMBER
+5. EXIT
+5
+*/
