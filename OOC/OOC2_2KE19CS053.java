0e3cecbcd983f36453ebcfcf4a77e1adbe2454c6
DarthMuzammil <muzammil.inamdar98@gmail.com>
2021-06-15 01:13:16 +0530

    Assignment 2

---
+++ OOC/OOC2_2KE19CS053.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+import java.lang.*;
+public class OOC2_2KE19CS053{
+    public static char enterchar(){
+        char ch;
+        Scanner  reader = new Scanner(System.in);
+        System.out.println("\nENTER A CHARACTER : ");
+        ch = reader.next().charAt(0);
+        return Character.toLowerCase(ch);
+    }
+    public static void main(String[] args){
+        Scanner  reader = new Scanner(System.in);
+        int choice = 0;
+        char ch;
+        while(choice != 5){
+        System.out.println("\nCHARACTER CHECK:\n1. VOWEL\n2. CONSONANT\n3. SPECIAL CHARACTER\n4. NUMBER\n5. EXIT");
+        choice = reader.nextInt();
+        if(choice != 5){
+            switch (choice) {
+                case 1:
+                    ch = enterchar();
+                    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                        System.out.println("\n" + ch + " is a vowel\n");
+                    }
+                    else{
+                        System.out.println("\n" + ch + " is not a vowel\n");
+                    }
+                    break;
+                case 2:
+                    ch = enterchar();
+                    if((ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') && ch > 'a' && ch < 'z'){
+                        System.out.println("\n" + ch + " is a consonant\n");
+                    }
+                    else{
+                        System.out.println("\n" + ch + " is not a consonant\n");
+                    }
+                    break;
+                case 3:
+                    ch = enterchar();
+                    if(!(ch >= 'a' && ch <= 'z') && !(ch > '0' && ch < '9')){
+                        System.out.println("\n" + ch + " is a special character\n");
+                    }
+                    else{
+                        System.out.println("\n" + ch + " is not a special character\n");
+                    }
+                    break;
+                case 4:
+                    ch = enterchar();
+                    if(ch >= '0' && ch <= '9'){
+                        System.out.println("\n" + ch + " is a number\n");
+                    }
+                    else{
+                        System.out.println("\n" + ch + " is not a number\n");
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+        reader.close();
+    }
+}
